886e71fcbd919ebc3265582bdb8796b003eed027
nooraproof <135523455+nooraproof@users.noreply.github.com>
2023-08-12 14:47:53 +0330

    Add files via upload

---
+++ Untictled1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct daneshjoo
+{
+int shomare;
+string nam;
+string reshte;
+}d;
+int main()
+{
+	d.shomare=125;
+	d.nam="noora";
+	d.reshte="math";
+	cout<<"shomare: \n"<<d.shomare;
+	cout<<"nam: \n"<<d.nam;
+	cout<<"reshte: \n"<<d.reshte;
+	
+	return 0;
+	
+	
+	
+}
